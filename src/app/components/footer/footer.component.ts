c914d4e73bd805aafa97f003c27b819e1038ce70
Iwona007 <iwonka.adamkiewicz@gmail.com>
2022-10-14 23:08:26 +0200

    code review: implement code review

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,7 +1,5 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
-import {AppRoute, ROUTES} from "../../routing/app-routes";
 
-// @ts-ignore
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
@@ -9,18 +7,11 @@ import {AppRoute, ROUTES} from "../../routing/app-routes";
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 
-
 export class FooterComponent {
-  routes: AppRoute[];
 
   date = new Date();
   year = [this.date.getFullYear()]
 
-
   constructor() {
-    this.routes = ROUTES;
-
   }
-
-
 }
