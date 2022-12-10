708eeca6df4f829dc9aa3c453711bd8ed40b4bf4
Dawid Stuzynski <dawid.stuzynski@gmail.com>
2022-12-10 12:12:12 +0100

    refactor: Make dialog serivice returning boolean

--- src/app/common/loading-modal/loading-snackbar.service.spec.ts
+++ src/app/common/loading-modal/loading-snackbar.service.spec.ts
@@ -1,12 +1,10 @@
 import { TestBed } from '@angular/core/testing';
 
 import { LoadingSnackbarService } from './loading-snackbar.service';
-import { MatSnackBar } from '@angular/material/snack-bar';
 import { MaterialModule } from '../../material.module';
 
 describe('LoadingDialogService', () => {
   let service: LoadingSnackbarService;
-  let snackBar: MatSnackBar;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
