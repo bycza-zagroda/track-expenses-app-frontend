6eaa3d5a0cf0542cbf1d79509ba2901c444911fb
Dawid Stuzynski <dawid.stuzynski@gmail.com>
2022-12-11 23:32:26 +0100

    test: Add test for deleteWallet

--- src/app/pages/wallets/my-wallets/pages-wallets-my-wallets.service.spec.ts
+++ src/app/pages/wallets/my-wallets/pages-wallets-my-wallets.service.spec.ts
@@ -1,20 +1,22 @@
 import { TestBed } from '@angular/core/testing';
 import { PagesWalletsMyWalletsService } from './pages-wallets-my-wallets.service';
-import SpyObj = jasmine.SpyObj;
 import { DomainsWalletsGateway } from '../../../domains/wallets/domains.wallets.gateway';
-import createSpyObj = jasmine.createSpyObj;
 import { of } from 'rxjs';
 import { MyWallet } from './pages-wallets-my-wallet.model';
 import { IWalletApiResponse } from '../../../domains/wallets/domains.wallets.types';
+import SpyObj = jasmine.SpyObj;
+import createSpyObj = jasmine.createSpyObj;
 
 describe('PagesWalletsMyWalletsService', () => {
   let service: PagesWalletsMyWalletsService;
   let gatewayMock: SpyObj<DomainsWalletsGateway>;
   let walletResp: IWalletApiResponse;
 
+  const wallet = new MyWallet({ creationDate: '2022-10-22T09:47:52.595721658Z', id: 12, name: 'wallet1' })
+
   beforeEach(async () => {
     walletResp = { creationDate: '2022-10-22T09:47:52.595721658Z', id: 12, name: 'wallet1' };
-    gatewayMock = createSpyObj<DomainsWalletsGateway>(DomainsWalletsGateway.name, ['getWallets']);
+    gatewayMock = createSpyObj<DomainsWalletsGateway>(DomainsWalletsGateway.name, ['getWallets', 'deleteWallet']);
 
     gatewayMock.getWallets.and.returnValue(of([walletResp]));
 
@@ -35,6 +37,14 @@ describe('PagesWalletsMyWalletsService', () => {
 
         done();
       });
+    });
+  });
+
+  describe('deleteWallet', () => {
+    it('should call gateway.deleteWallet()', () => {
+
+      service.deleteWallet(wallet);
+      expect(gatewayMock.deleteWallet).toHaveBeenCalledWith(wallet.id!)
     });
   });
 });
