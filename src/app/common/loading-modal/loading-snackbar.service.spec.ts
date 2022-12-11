86a9f8cd16789d4b2ec15e559fe183928fd06161
Dawid Stuzynski <dawid.stuzynski@gmail.com>
2022-12-11 22:32:35 +0100

    test: Add tests for ConfirmDialogService

--- src/app/common/loading-modal/loading-snackbar.service.spec.ts
+++ src/app/common/loading-modal/loading-snackbar.service.spec.ts
@@ -1,22 +1,31 @@
 import { TestBed } from '@angular/core/testing';
 
 import { LoadingSnackbarService } from './loading-snackbar.service';
-import { MaterialModule } from '../../material.module';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import createSpyObj = jasmine.createSpyObj;
 
 describe('LoadingDialogService', () => {
   let service: LoadingSnackbarService;
+  let matSnackMock: MatSnackBar;
 
   beforeEach(() => {
+    matSnackMock = createSpyObj<MatSnackBar>(MatSnackBar.name, ['open']);
     TestBed.configureTestingModule({
-      imports: [MaterialModule],
       providers: [
         LoadingSnackbarService,
+        { provide: MatSnackBar, useValue: matSnackMock },
       ],
     });
     service = TestBed.inject(LoadingSnackbarService);
   });
 
+  it('should throw error if hide() is called before show()', () => {
+    expect(() => service.hide())
+        .toThrow(new Error('hide() called before showing loading indicator'))
+  });
+
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
 });
