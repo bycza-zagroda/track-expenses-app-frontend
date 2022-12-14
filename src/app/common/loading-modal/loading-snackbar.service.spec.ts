481d07628f8c56aa9ef74309f70b9a3647b54786
Dawid Stuzynski <dawid.stuzynski@gmail.com>
2022-12-14 21:16:43 +0100

    test: Add tests for LoadingSnackbarService

--- src/app/common/loading-modal/loading-snackbar.service.spec.ts
+++ src/app/common/loading-modal/loading-snackbar.service.spec.ts
@@ -1,15 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 
 import { LoadingSnackbarService } from './loading-snackbar.service';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatSnackBar, MatSnackBarRef } from '@angular/material/snack-bar';
+import { LoadingModalComponent } from './loading-modal.component';
 import createSpyObj = jasmine.createSpyObj;
+import SpyObj = jasmine.SpyObj;
 
 describe('LoadingDialogService', () => {
   let service: LoadingSnackbarService;
-  let matSnackMock: MatSnackBar;
+  let matSnackMock: SpyObj<MatSnackBar>;
+  let snackBarRefMock: SpyObj<MatSnackBarRef<LoadingModalComponent>>;
 
   beforeEach(() => {
-    matSnackMock = createSpyObj<MatSnackBar>(MatSnackBar.name, ['open']);
+    matSnackMock = createSpyObj<MatSnackBar>(MatSnackBar.name, ['openFromComponent', 'open']);
+    snackBarRefMock = createSpyObj<MatSnackBarRef<LoadingModalComponent>>(MatSnackBarRef.name, ['dismiss']);
+    matSnackMock.openFromComponent.and.returnValue(snackBarRefMock);
+
     TestBed.configureTestingModule({
       providers: [
         LoadingSnackbarService,
@@ -22,6 +28,18 @@ describe('LoadingDialogService', () => {
   it('should throw error if hide() is called before show()', () => {
     expect(() => service.hide())
         .toThrow(new Error('hide() called before showing loading indicator'))
+  });
+
+  it('should open loading dialog', () => {
+    service.show('message');
+    expect(matSnackMock.openFromComponent).toHaveBeenCalled();
+  });
+
+  it('should hide loading dialog after open it', () => {
+    service.show('')
+    service.hide()
+    expect(snackBarRefMock.dismiss).toHaveBeenCalled()
+
   });
 
   it('should be created', () => {
