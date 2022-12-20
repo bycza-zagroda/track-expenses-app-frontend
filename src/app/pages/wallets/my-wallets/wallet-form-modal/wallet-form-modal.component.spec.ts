a1173d08c93313a7c7c32a7e56e719528c38d1af
Tomek <tomek1989gw@wp.pl>
2022-12-20 09:32:08 +0100

    basic WalletModal tests

--- src/app/pages/wallets/my-wallets/wallet-form-modal/wallet-form-modal.component.spec.ts
+++ src/app/pages/wallets/my-wallets/wallet-form-modal/wallet-form-modal.component.spec.ts
@@ -1,10 +1,11 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, flush, flushMicrotasks, TestBed } from '@angular/core/testing';
 
 import { WalletFormModalComponent } from './wallet-form-modal.component';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from '../../../../material.module';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { By } from '@angular/platform-browser';
 
 describe('WalletFormModalComponent', () => {
   let component: WalletFormModalComponent;
@@ -33,4 +34,36 @@ describe('WalletFormModalComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('Should display error message when invalid data is provided', () => {
+
+    it('Empty name', () => {
+      component.form.get('name')?.setValue('');
+
+      fixture.detectChanges();
+
+      const errorMessageDiv = fixture.debugElement.query(By.css('.mat-error')); // null :(
+      expect(errorMessageDiv.nativeElement.textContent).toBe('Name is required');
+    });
+
+    it('Name with more than 20 characters', () => {
+
+      component.form.setValue({
+        name: "asdasdsadasdasdasdasdasdasdasdsad",
+      })
+      //flushMicrotasks(); // fakeAsync tez nei dziala :(
+      fixture.detectChanges();
+
+      const errorMessageDiv = fixture.debugElement.query(By.css('.mat-error')); // null :(
+
+      expect(errorMessageDiv.nativeElement.textContent).toBe('Name is required');
+    });
+  })
+
+
+
+
+
+
+
 });
--- src/app/pages/wallets/my-wallets/wallet-form-modal/wallet-form-modal.component.ts
+++ src/app/pages/wallets/my-wallets/wallet-form-modal/wallet-form-modal.component.ts
@@ -17,16 +17,27 @@ export class WalletFormModalComponent {
         @Inject(MAT_DIALOG_DATA) public data: MyWallet | undefined,
     ) {
         this.form = new FormGroup<IWalletModalFormType>({
-            name: new FormControl(this.data?.name ?? '', { validators: Validators.required, nonNullable: true }),
+            name: new FormControl(this.data?.name ?? '', {
+              validators: [
+                Validators.required,
+                Validators.maxLength(20),
+              ],
+              nonNullable: true,
+            }),
         });
     }
 
-    public showNameError(): boolean | undefined {
-      return this.form.get('name')?.invalid && this.form.get('name')?.touched;
+    public showNameError(): boolean {
+      return this.form.get('name')?.invalid && this.form.get('name')?.touched && this.form.get('name')?.errors?.['required'];
+    }
+
+    public showMaxLengthError(): boolean {
+      return this.form.get('name')?.invalid && this.form.get('name')?.touched && this.form.get('name')?.errors?.['maxlength'];
     }
 
     public save(): void {
-        if(this.form.valid) {
+      console.log(this.form.get('name'))
+        if(this.form.invalid) {
           return;
         }
 
