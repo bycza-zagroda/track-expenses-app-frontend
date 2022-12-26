e261dd8997064caf15f6b8a2a9140fb7c4f5a4f6
tomektomek321 <71927596+tomektomek321@users.noreply.github.com>
2022-12-26 17:12:50 +0100

    Wallet Form Modal name valudation (#53)
    * Wallet Form Modal name valudation
    
    * PR fixes
    
    * small fix
    
    * basic WalletModal tests
    
    * PR fixes 2
    
    * Another Fix 3
    
    * wallet-form-modal fixes
    
    * small test fix

--- src/app/pages/wallets/management/wallet-editor/pages-wallets-management-editor.component.spec.ts
+++ src/app/pages/wallets/management/wallet-editor/pages-wallets-management-editor.component.spec.ts
@@ -1,18 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PagesWalletsManagementEditorComponent } from './pages-wallets-management-editor.component';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from '../../../../material.module';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { By } from '@angular/platform-browser';
 
 describe('PagesWalletsManagementEditorComponent', () => {
   let component: PagesWalletsManagementEditorComponent;
   let fixture: ComponentFixture<PagesWalletsManagementEditorComponent>;
+  let ERROR_MESSAGE_REQUIRED: string;
+  let ERROR_MESSAGE_MAXLENGTH: string;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PagesWalletsManagementEditorComponent ],
+      declarations: [PagesWalletsManagementEditorComponent],
       imports: [
         ReactiveFormsModule,
         MaterialModule,
@@ -23,7 +25,7 @@ describe('PagesWalletsManagementEditorComponent', () => {
         { provide: MAT_DIALOG_DATA, useValue: undefined },
       ],
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(PagesWalletsManagementEditorComponent);
     component = fixture.componentInstance;
@@ -33,4 +35,33 @@ describe('PagesWalletsManagementEditorComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('form validation', () => {
+    beforeEach(() => {
+      ERROR_MESSAGE_REQUIRED = 'Name is required'
+      ERROR_MESSAGE_MAXLENGTH = 'Name can\'t have more than 20 characters'
+    })
+
+    it('should show validation error if name input is empty', () => {
+      component.form.get('name')?.setValue('');
+      component.form.get('name')?.markAsTouched();
+
+      fixture.detectChanges();
+
+      const errorMessageDiv = fixture.debugElement.query(By.css('.mat-error'));
+      const errorMessageText: HTMLDivElement = errorMessageDiv.nativeElement as HTMLDivElement;
+      expect(errorMessageText.textContent!.trim()).toBe(ERROR_MESSAGE_REQUIRED);
+    });
+
+    it('should show validation error if name input has more than 20 characters', () => {
+      component.form.get('name')?.setValue('Text have more than twenty characters for sure');
+      component.form.get('name')?.markAsTouched();
+
+      fixture.detectChanges();
+
+      const errorMessageDiv = fixture.debugElement.query(By.css('.mat-error'));
+      const errorMessageText: HTMLDivElement = errorMessageDiv.nativeElement as HTMLDivElement;
+      expect(errorMessageText.textContent!.trim()).toBe(ERROR_MESSAGE_MAXLENGTH);
+    });
+  })
 });
--- src/app/pages/wallets/management/wallet-editor/pages-wallets-management-editor.component.ts
+++ src/app/pages/wallets/management/wallet-editor/pages-wallets-management-editor.component.ts
@@ -1,6 +1,6 @@
 import { Component, Inject } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA }  from '@angular/material/dialog';
-import { FormControl, FormGroup } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { WalletsManagementItem } from '../pages-wallets-wallets-management-item.model';
 import { IWalletModalFormType } from './pages-wallets-management-editor.types';
 
@@ -10,22 +10,44 @@ import { IWalletModalFormType } from './pages-wallets-management-editor.types';
   styleUrls: ['./pages-wallets-management-editor.component.scss'],
 })
 export class PagesWalletsManagementEditorComponent {
-    public form!: FormGroup<IWalletModalFormType>;
+  public form!: FormGroup<IWalletModalFormType>;
 
-    public constructor(
-        private readonly dialogRef: MatDialogRef<PagesWalletsManagementEditorComponent>,
-        @Inject(MAT_DIALOG_DATA) public data: WalletsManagementItem | undefined,
-    ) {
-        this.form = new FormGroup<IWalletModalFormType>({
-            name: new FormControl(this.data?.name ?? '', { nonNullable: true }),
-        });
+  public constructor(
+    private readonly dialogRef: MatDialogRef<PagesWalletsManagementEditorComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: WalletsManagementItem | undefined,
+  ) {
+    this.form = new FormGroup<IWalletModalFormType>({
+      name: new FormControl(this.data?.name ?? '', {
+        validators: [
+          Validators.required,
+          Validators.maxLength(20),
+        ],
+        nonNullable: true,
+      }),
+    });
+  }
+
+  public get nameIsNotProvided(): boolean {
+    return this.checkInputError('name', 'required');
+  }
+
+  public get nameHasMoreThan20Characters(): boolean {
+    return this.checkInputError('name', 'maxlength');
+  }
+
+  public checkInputError(inputName: string, errorType: string): boolean {
+    return !!(this.form.get(inputName)?.invalid && this.form.get(inputName)?.touched && this.form.get(inputName)?.errors?.[errorType]);
+  }
+
+  public save(): void {
+    if (this.form.invalid) {
+      return;
     }
 
-    public save(): void {
-        this.dialogRef.close(this.form.value);
-    }
+    this.dialogRef.close(this.form.value);
+  }
 
-    public cancel(): void {
-        this.dialogRef.close(null);
-    }
+  public cancel(): void {
+    this.dialogRef.close(null);
+  }
 }
