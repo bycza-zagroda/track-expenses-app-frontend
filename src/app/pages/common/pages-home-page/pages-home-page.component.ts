d5b16e56834be68f9980215f216a2a735d9901dc
Bartexs <barttosz.witkowski@gmail.com>
2023-02-04 22:41:18 +0100

    Created home page component

---
+++ src/app/pages/common/pages-home-page/pages-home-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pages-home-page',
+  templateUrl: './pages-home-page.component.html',
+  styleUrls: [ './pages-home-page.component.scss' ],
+})
+export class PagesHomePageComponent {
+}
