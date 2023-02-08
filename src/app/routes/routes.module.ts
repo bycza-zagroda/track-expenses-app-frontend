fc1aaaab728b0abbb3bf8af285b1dfe21b647a41
Bartexs <barttosz.witkowski@gmail.com>
2023-02-08 16:48:48 +0100

    Added wildard route to show 404 page when url not matched

--- src/app/routes/routes.module.ts
+++ src/app/routes/routes.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PagesHomePageComponent } from '../pages/common/pages-home-page/pages-home-page.component';
+import { PagesNotFoundPageComponent } from '../pages/common/pages-not-found-page/pages-not-found-page.component';
 
 const routes: Routes = [
   { path: '', component: PagesHomePageComponent },
@@ -8,6 +9,7 @@ const routes: Routes = [
     path: 'wallets',
     loadChildren: () => import('../pages/wallets/pages-wallets.module').then(m => m.PagesWalletsModule),
   },
+  { path: '**', component: PagesNotFoundPageComponent },
 ];
 
 @NgModule({
