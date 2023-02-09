9c623485e2a4afd61944c68fcf3a417359f851bd
Bartexs <barttosz.witkowski@gmail.com>
2023-02-09 19:57:39 +0100

    Moved not-found pages to pages folder, removed not needed divs, switched to more suitable alt name for 404 image and increased h1 size

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,8 +11,8 @@ import { RouterModule } from '@angular/router';
 import { RoutesModule } from './routes/routes.module';
 import { CommonComponentsModule } from './common/components/common-components.module';
 import { HttpClientModule } from '@angular/common/http';
-import { PagesHomePageComponent } from './pages/common/pages-home-page/pages-home-page.component';
-import { PagesNotFoundPageComponent } from './pages/common/pages-not-found-page/pages-not-found-page.component';
+import { PagesHomePageComponent } from './pages/home/pages-home-page.component';
+import { PagesNotFoundPageComponent } from './pages/not-found/pages-not-found-page.component';
 
 @NgModule({
   declarations: [ AppComponent, PagesHomePageComponent, PagesNotFoundPageComponent ],
--- src/app/routes/routes.module.ts
+++ src/app/routes/routes.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PagesHomePageComponent } from '../pages/common/pages-home-page/pages-home-page.component';
-import { PagesNotFoundPageComponent } from '../pages/common/pages-not-found-page/pages-not-found-page.component';
+import { PagesHomePageComponent } from '../pages/home/pages-home-page.component';
+import { PagesNotFoundPageComponent } from '../pages/not-found/pages-not-found-page.component';
 
 const routes: Routes = [
   { path: '', component: PagesHomePageComponent },
