94b117fab4997adbfbe361ebbe1340cb0c18f692
Bartexs <barttosz.witkowski@gmail.com>
2023-02-20 23:42:53 +0100

    Created component to show notification when transactions list is empty

---
+++ src/app/pages/wallets/wallet-details/empty-transaction-list/pages-wallet-details-empty-transaction-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pages-wallet-details-empty-transaction-list',
+  templateUrl: './pages-wallet-details-empty-transaction-list.component.html',
+  styleUrls: [ './pages-wallet-details-empty-transaction-list.component.scss' ],
+})
+export class PagesWalletDetailsEmptyTransactionListComponent {
+}
