b2414ffb0bb4102e7de033d3913ca54c1bff5bfc
Bartexs <barttosz.witkowski@gmail.com>
2023-02-25 15:49:56 +0100

    Moved and renamed no-results reusable component

---
+++ src/app/common/components/no-results/no-results.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-no-results',
+  templateUrl: './no-results.component.html',
+  styleUrls: [ './no-results.component.scss' ], 
+})
+export class NoResultsComponent {
+}
--- src/app/pages/wallets/wallet-details/empty-transaction-list/pages-wallet-details-empty-transaction-list.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-pages-wallet-details-empty-transaction-list',
-  templateUrl: './pages-wallet-details-empty-transaction-list.component.html',
-  styleUrls: [ './pages-wallet-details-empty-transaction-list.component.scss' ],
-})
-export class PagesWalletDetailsEmptyTransactionListComponent {
-}
