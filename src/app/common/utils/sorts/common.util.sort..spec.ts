033a0e43fc36f799fd85dad1d02125f6a0a16672
Tomek <tomek1989gw@wp.pl>
2023-03-22 22:10:56 +0000

    pr fixes

--- src/app/common/utils/sorts/common.util.sort..spec.ts
+++ src/app/common/utils/sorts/common.util.sort..spec.ts
@@ -1,5 +1,7 @@
 import { transactionCategoriesObjectsMockFunc } from 'src/app/domains/categories/domains.transaction-categories.mocks';
-import { sortAlphabeticallyByProp } from './common.util.sort.';
+import { WALLET_TRANSACTIONS_OBJECTS_MOCK } from 'src/app/domains/transactions/domains.transactions.mocks';
+import { WalletTransaction } from 'src/app/pages/wallets/wallet-details/pages-wallet-details-item.model';
+import { sortAlphabeticallyByProp, sortByDate } from './common.util.sort.';
 
 describe('utils sorts', () => {
   describe('sortAlphabeticallyByProp', () => {
@@ -11,6 +13,28 @@ describe('utils sorts', () => {
       expect(sortedTab[0].name).toBe(tab[1].name);
       expect(sortedTab[1].name).toBe(tab[2].name);
       expect(sortedTab[2].name).toBe(tab[0].name);
+    });
+  });
+
+  describe('sortByDate', () => {
+    it('should return sorted array', () => {
+      const tab: WalletTransaction[] = WALLET_TRANSACTIONS_OBJECTS_MOCK(1);
+
+      const sortedTab = sortByDate(WALLET_TRANSACTIONS_OBJECTS_MOCK(1), 'date');
+
+      expect(sortedTab[0].date).toEqual(tab[0].date);
+      expect(sortedTab[1].date).toEqual(tab[2].date);
+      expect(sortedTab[2].date).toEqual(tab[1].date);
+    });
+
+    it('should return sorted array descending', () => {
+      const tab: WalletTransaction[] = WALLET_TRANSACTIONS_OBJECTS_MOCK(1);
+
+      const sortedTab = sortByDate(WALLET_TRANSACTIONS_OBJECTS_MOCK(1), 'date', true);
+
+      expect(sortedTab[0].date).toEqual(tab[1].date);
+      expect(sortedTab[1].date).toEqual(tab[2].date);
+      expect(sortedTab[2].date).toEqual(tab[0].date);
     });
   });
 });
