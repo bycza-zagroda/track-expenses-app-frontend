69c8ee7889e8505b71a3d2d7e5f6ef07f6ba0182
Adrian Romański <muszynianka1337@gmail.com>
2023-07-21 07:59:22 +0200

    feature: json-server configuration with basic wallets data

---
+++ apps/teaf-ng/mock/json-server/data/wallets.data.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    id: 4,
+    name: 'Wallet four',
+    creationDate: '2020-05-20T09:47:52Z',
+    userId: null,
+  },
+  {
+    id: 1,
+    name: 'Wallet one',
+    creationDate: '2022-10-22T09:47:52Z',
+    userId: null,
+  },
+  {
+    id: 3,
+    name: 'Wallet three',
+    creationDate: '2021-09-21T09:47:52Z',
+    userId: null,
+  },
+  {
+    id: 2,
+    name: 'Wallet two',
+    creationDate: '2021-02-19T09:47:52Z',
+    userId: null,
+  },
+];
